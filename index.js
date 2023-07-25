346c4fa596efd12f14078de335ef90e8f2322e5b
rahulavashist <111230814+rahulavashist@users.noreply.github.com>
2023-07-25 12:29:53 +0530

    Modify code with promises or async or await

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-function RandomNumber(count) {
-  const RNumbers = [];
-  let Sum = 0;
-  let mindelay = 500;
-  let maxdelay = 10000;
+// function RandomNumber(count) {
+//   const RNumbers = [];
+//   let Sum = 0;
+//   let mindelay = 500;
+//   let maxdelay = 10000;
 
-  const genRandomNumber = () => {
-    const Number = Math.floor(Math.random() * 100);
+//   const genRandomNumber = () => {
+//     const Number = Math.floor(Math.random() * 100);
 
-    console.log('-->>', Number);
+//     console.log('-->>', Number);
 
-    RNumbers.push(Number);
-    Sum += Number;
-    console.log('Total =>', Sum);
-  };
-  for (let i = 0; i < count; i++) {
-    const randomDelay = Math.floor(
-      Math.random() * maxdelay - mindelay + 1 + mindelay
-    );
+//     RNumbers.push(Number);
+//     Sum += Number;
+//     console.log('Total =>', Sum);
+//   };
+//   for (let i = 0; i < count; i++) {
+//     const randomDelay = Math.floor(
+//       Math.random() * maxdelay - mindelay + 1 + mindelay
+//     );
 
-    setTimeout(() => genRandomNumber(i), randomDelay);
-  }
+//     setTimeout(() => genRandomNumber(i), randomDelay);
+//   }
+// }
+
+// const count = 10;
+// RandomNumber(count);
+
+
+
+////////--------///////
+
+
+
+function generateRandomNumber() {
+  return new Promise((resolve) => {
+    const randomDelay = Math.floor(Math.random() * 1000 + 100);
+
+    setTimeout(() => {
+      const number = Math.floor(Math.random() * 100);
+      resolve(number)
+      
+    }, randomDelay);
+  });
 }
 
-const count = 10;
-RandomNumber(count);
+async function generateRandomNumberArray(count) {
+  const arr = [];
+
+  for (let i = 0; i < count; i++) {
+    arr.push(generateRandomNumber());
+  }
+
+  return Promise.all(arr);
+}
+
+ async function sum(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+ async function RandomNumber(){
+  try {
+          const number = await generateRandomNumberArray(10)
+          console.log(sum(number));
+  } catch (error) {
+          console.log(error);
+  }
+ }
+ RandomNumber()
+
